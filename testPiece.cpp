abff6e578b48d82f9f9735628129af7e3eff2d3e
Jeremy Busch <jcbusch98@gmail.com>
2022-05-06 19:58:04 -0600

    test piece

--- testPiece.cpp
+++ testPiece.cpp
@@ -38,8 +38,8 @@ class testPiece
 		assert(pawn.position == 'f3');
 	}	//teardown
 
-	
-	
+
+
 	void testAssign_piece_queen() {
 		//setup
 		Pawn pawn = pawn('e', '8', true);
@@ -103,12 +103,12 @@ class testPiece
 		Pawn pawn = pawn('b', '2', true);
 		//exercise
 		Board board = new Board()
-		
+
 			// TODO : ask brother helfrich how the pawn mopves and how nMoves is incremented
-		
-			
-			
-		int nMoves = pawn.getNMoves();
+
+
+
+			int nMoves = pawn.getNMoves();
 		//verify
 		assert(nMoves == 1);
 	}	//teardown
@@ -119,13 +119,13 @@ class testPiece
 		Pawn pawn = pawn('b', '2', true);
 		//exercise
 		Board board = new Board()
-			
-			
+
+
 			// TODO : ask brother helfrich how the pawn mopves and how nMoves is incremented
 
 
 
-		int nMoves = pawn.getNMoves();
+			int nMoves = pawn.getNMoves();
 		//verify
 		assert(nMoves == 10);
 	}	//teardown
@@ -155,11 +155,11 @@ class testPiece
 		//verify
 		assert(pawn.getPosition() == 'f3');
 	}	//teardown
-	
+
 
 	//TODO: need to test justMoved
 
-
+	// should only need one of these
 	void testgetLetter_pawn() {
 		//setup
 		//exercise
@@ -170,15 +170,152 @@ class testPiece
 
 
 
+
+// starting_white
+// # # a b c d e f g h # #
+// #							 #
+// 1   r k b q k b k r   1
+// 2   p p p p p p p p   2
+// 3  						 3
+// 4  						 4
+// 5							 5
+// 6							 6
+// 7   p p p p(p)p p p   7
+// 8   r k b q k b k r   8
+// #							 #
+// # # a b c d e f g h # #
+	void testgetMoves_startingWhite() {
+		//setup
+		Board board = new Board();
+		Pawn piece = new Pawn('e', '7', true);
+		//exercise
+		String moves[] = piece.getMoves(board);
+		//verify
+		assert(moves == ["e6", "e5"]);
+	}	//teardown
+
+
+	// capture_white
+// # # a b c d e f g h # #
+// #							 #
+//	1   r k b q k b k r   1
+//	2   p p p p   p p p   2
+//	3  						 3
+//	4  			p   		 4
+//	5			    (p)	    5
+//	6							 6
+//	7   p p p p p   p p   7
+//	8   r k b q k b k r   8
+// #							 #
+// # # a b c d e f g h # #
+	void testgetMoves_captureWhite() {
+		//setup
+		Board board = new Board();
+		Pawn piece = new Pawn('f', '5', true);
+		//exercise
+		String moves[] = piece.getMoves(board);
+		//verify
+		assert(moves == ["e4", "f4"]);
+	}	//teardown
+
+
+	// en-passant_white
+// # # a b c d e f g h # #
+// #							 #
+//	1   r k b q k b k r   1
+//	2   p p p   p p p p   2
+//	3  						 3
+//	4  		 p(p)		    4
+//	5							 5
+//	6							 6
+//	7   p p p p   p p p   7
+//	8   r k b q k b k r   8
+// #							 #
+// # # a b c d e f g h # #
 	void testgetMoves_() {
 		//setup
+		Board board = new Board();
+		Pawn piece = new Pawn('e', '4', true);
 		//exercise
-		Pawn pawn;
+		String moves[] = piece.getMoves(board);
 		//verify
-		assert(pawn.getLetter() == 'p');
+		assert(moves == ["d3", "e3"]);
+	}	//teardown
+
+
+	// starting_black
+// # # a b c d e f g h # #
+// #							 #
+//	1   r k b q k b k r   1
+//	2   p p p p(p)p p p   2
+//	3  						 3
+//	4  						 4
+//	5							 5
+//	6							 6
+//	7   p p p p p p p p   7
+//	8   r k b q k b k r   8
+// #							 #
+// # # a b c d e f g h # #
+	void testgetMoves_blackStarting() {
+		//setup
+		Board board = new Board();
+		Pawn piece = new Pawn('e', '2', true);
+		//exercise
+		String moves[] = piece.getMoves(board);
+		//verify
+		assert(moves == ["e3", "e4"]);
 	}	//teardown
 
 
 
-	
+
+// en-passant_black
+// # # a b c d e f g h # #
+// #							 #
+// 1   r k b q k b k r   1
+// 2   p p p   p p p p   2
+// 3  						 3
+// 4  		(p)p		    4
+// 5							 5
+// 6							 6
+// 7   p p p p   p p p   7
+// 8   r k b q k b k r   8
+// #							 #
+// # # a b c d e f g h # #
+	void testgetMoves_blackEnPassant() {
+		//setup
+		Board board = new Board();
+		Pawn piece = new Pawn('d', '4', true);
+		//exercise
+		String moves[] = piece.getMoves(board);
+		//verify
+		assert(moves == ["e5", "d5"]);
+	}	//teardown
+
+
+
+// capture_black
+// # # a b c d e f g h # #
+// #							 #
+// 1   r k b q k b k r   1
+// 2   p p p p   p p p   2
+// 3  						 3
+// 4  		  (p)  		 4
+// 5			     p		 5
+// 6							 6
+// 7   p p p p p   p p   7
+// 8   r k b q k b k r   8
+// #							 #
+// # # a b c d e f g h # #
+	void testgetMoves_captureBlack() {
+		//setup
+		Board board = new Board();
+		Pawn piece = new Pawn('e', '4', true);
+		//exercise
+		String moves[] = piece.getMoves(board);
+		//verify
+		assert(moves == ["f5", "e5"]);
+	}	//teardown
+
+
 };
